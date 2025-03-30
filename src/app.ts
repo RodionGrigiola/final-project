5b79ea3ca3021d741ea308823cf752df03470760
Rodion Grigiola <rogrigiola@gmail.com>
2025-03-30 21:14:13 +0200

    add serving of public folder items

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express, { Request, Response } from "express";
 import furnitureCategoryRouter from "./routes/furnitureCategoryRoute";
 import furnitureItemsRouter from "./routes/furnitureItemsRoute";
 import userRouter from "./routes/userRoute";
+import path from 'node:path';
 
 const app = express();
+
+app.use('/models', express.static(path.join(__dirname, 'public/models')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
